ed7c78fca6d0792c5fe2b2d90e52b722a7f242e8
Brendon F. <github.p0u9u@passinbox.com>
2024-11-25 15:04:59 -0300

    feat: create the basic vite + react.tsx structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold text-center p-10 text-slate-600">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
